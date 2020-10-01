43a30461701159a1c952d1ebb165d34df96b4121
carolZapata <carozapatac@gmail.com>
2020-10-01 11:50:52 +0200

    [ADD] Paquete dto con la clase Persona

---
+++ src/com/carol/datos/ConsultaDatos.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.carol.datos;
+
+/**
+ * @author carol
+ *
+ */
+public class ConsultaDatos {
+
+}
---
+++ src/com/carol/dto/Persona.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.carol.dto;
+
+/**
+ * @author carol
+ *
+ */
+public class Persona {
+	private String nombre;
+	private double peso;
+	private double altura;
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the peso
+	 */
+	public double getPeso() {
+		return peso;
+	}
+	/**
+	 * @param peso the peso to set
+	 */
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	/**
+	 * @return the altura
+	 */
+	public double getAltura() {
+		return altura;
+	}
+	/**
+	 * @param altura the altura to set
+	 */
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", peso=" + peso + ", altura=" + altura + "]";
+	}
+	
+	
+}
